ec2ee55c7d78515ab0f58dcc954cf3393fef7b40
haorandai <kevdaup@bu.edu>
2024-02-20 17:37:28 -0800

    Selection Sorting Implementation

--- src/Sorting/SelectionSort.java
+++ src/Sorting/SelectionSort.java
@@ -1,8 +1,27 @@
-package Sorting;/**
+package Sorting;
+
+/**
  * @author: Haoran Dai
  * @email: haorand16@gmail.com
  * @date: 2/20/24 17:20
- * @description: some desc
+ * @description: Selection Sort Algorithm Implementation
  */
 public class SelectionSort {
+    public static void main(int[] arr) {
+        if (arr == null || arr.length < 2) return;
+        int minIndex = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            minIndex = i;
+            for (int j = i; j < arr.length; j++) {
+                minIndex = arr[j] < arr[minIndex] ? j : minIndex;
+            }
+            swap(arr, i, minIndex);
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j){
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
 }
